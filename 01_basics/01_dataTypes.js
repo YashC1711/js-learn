255812c123d3447815c959cc7ac8a02749388188
Yash Chinawale <110768029+YashC1711@users.noreply.github.com>
2025-03-05 04:24:24 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all ur js code in newer version --> no need to write but good practice
+
+//alert("hello"); // we are using nodejs not browser
+console.log(3+
+    3); //code readebality should be high
+console.log("Yash");
+
+let name ="yash"
+let age=22;
+let isloggin = false
+let state; //undefined
+let taste = null; //null
+
+//number ==> 2 to power 53 range
+//bigInt
+//string => ""
+//boolean => true / false
+//null  => standalone value 
+//undefined => value not assigned
+//symbol (in react) => unique 
+
+
+//object 
+
+console.log(typeof("yash"));
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
