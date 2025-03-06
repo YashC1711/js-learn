6e309ec91dc1a8e3114c0a4a5c38e8c2be02a795
Yash Chinawale <110768029+YashC1711@users.noreply.github.com>
2025-03-06 03:51:49 +0000

    conversion

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1) //true convert the string to number 
+
+// avoid this type of comparision
+// console.log(null > 0)
+// console.log(null >= 0) 
+
+
+//==    and ===
+// console.log("2" == 2)  //convert to number 
+// console.log("2" === 2) // no conversion of datatype in ===
